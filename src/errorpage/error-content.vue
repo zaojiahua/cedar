367965bd682c2252e1aad45dcba54d3110797637
tingting <1140115803@qq.com>
2019-03-19 10:40:10 +0800

    Fixing error_page components

--- src/errorpage/error-content.vue
+++ src/errorpage/error-content.vue
@@ -11,9 +11,8 @@
 </template>
 
 <script>
-    import './error.less'
     export default {
-        name: 'error_content',
+        name: 'errorContent',
         props: {
             code: String,
             desc: String,
@@ -21,3 +20,47 @@
         }
     }
 </script>
+
+<style scoped>
+    .error-page {
+        width: 100%;
+        height: 100%;
+        /*position: relative;*/
+        background: #f8f8f9;
+    }
+    .content-con {
+        width: 700px;
+        height: 600px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -60%);
+    }
+    img{
+        display: block;
+        width: 100%;
+        height: 100%;
+    }
+    .text-con {
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+    h4{
+        position: absolute;
+        left: 0;
+        top: 0;
+        font-size: 80px;
+        font-weight: 700;
+        color: #348EED;
+    }
+    h5{
+        position: absolute;
+        width: 700px;
+        left: 0;
+        top: 100px;
+        font-size: 20px;
+        font-weight: 700;
+        color: #67647D;
+    }
+</style>
