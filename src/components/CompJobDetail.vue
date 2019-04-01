6dd01bfdfe25353fdf13cc000e21483a1d70b82f
tingting <1140115803@qq.com>
2019-04-01 09:56:48 +0800

    jobDetail模块加上Job数据的获取

--- src/components/CompJobDetail.vue
+++ src/components/CompJobDetail.vue
@@ -1,52 +1,54 @@
 <template>
     <Card title="用例详细信息">
         <Form :label-width="80">
-            <FormItem v-model="jonInfo">
+            <FormItem v-model="jobInfo">
                 <b slot="label">ID:</b>
-                <Input v-model="jonInfo.id" disabled="true" class="disabled-input">
+                <Input v-model="jobInfo.id" disabled="true" class="disabled-input">
             </FormItem>
             <FormItem>
                 <b slot="label">用例名称:</b>
-                <Input v-model="jonInfo.job_name" disabled="true" class="disabled-input">
+                <Input v-model="jobInfo.job_name" disabled="true" class="disabled-input">
             </FormItem>
             <FormItem>
                 <b slot="label">用例说明:</b>
-                <Input v-model="jonInfo.description" disabled="true" class="disabled-input">
+                <Input v-model="jobInfo.description" disabled="true" class="disabled-input">
             </FormItem>
             <FormItem>
                 <b slot="label">测试用途:</b>
-                <Input v-model="jonInfo.test_area" disabled="true" class="disabled-input">
+                <Input v-model="testArea" disabled="true" class="disabled-input">
             </FormItem>
             <FormItem>
                 <b slot="label">适配机型:</b>
-                <Input v-model="jonInfo.phone_model_name" disabled="true" class="disabled-input">
+                <Input v-model="phoneModels" disabled="true" class="disabled-input">
             </FormItem>
             <FormItem>
                 <b slot="label">安卓版本:</b>
-                <Input v-model="jonInfo.android_version" disabled="true" class="disabled-input">
+                <Input v-model="androidVersion" disabled="true" class="disabled-input">
             </FormItem>
             <FormItem>
                 <b slot="label">ROM版本:</b>
-                <Input v-model="jonInfo.rom_version" disabled="true" class="disabled-input">
+                <Input v-model="romVersion" disabled="true" class="disabled-input">
             </FormItem>
             <FormItem>
                 <b slot="label">自定义标签:</b>
-                <Input v-model="jonInfo.custom_tag" disabled="true" class="disabled-input">
+                <Input v-model="customTag" disabled="true" class="disabled-input">
             </FormItem>
             <FormItem>
                 <b slot="label">编写人员:</b>
-                <Input v-model="jonInfo.author" disabled="true" class="disabled-input">
+                <Input v-model="jobInfo.author.username" disabled="true" class="disabled-input">
             </FormItem>
             <FormItem>
                 <b slot="label">更新时间:</b>
-                <Input v-model="jonInfo.updated_time" disabled="true" class="disabled-input">
+                <Input v-model="jobInfo.updated_time" disabled="true" class="disabled-input">
             </FormItem>
         </Form>
     </Card>
 </template>
 
 <script>
-    const serializer = [{
+    import utils from "../lib/utils"
+
+    const serializer = {
         android_version:[{
             id:"number",
             version:"string"
@@ -77,23 +79,72 @@
         }],
         updated_time:"string"
 
-    }]
+    };
 
     export default {
         data(){
             return{
-                jonInfo:{
-                    id:"",
-                    job_name:"",
-                    description:"",
-                    test_area:"",
-                    phone_model_name:"",
-                    android_version: "",
-                    rom_version: "",
-                    author:"",
-                    updated_time: "",
-                    custom_tag: ""
-                }
+                jobInfo:utils.validate(serializer, {}),
+                androidVersion: "",
+                romVersion: "",
+                customTag: "",
+                phoneModels: "",
+                testArea: ""
+            }
+        },
+        methods:{
+            jobRefresh(jobId){
+                this.$ajax
+                    .get(
+                        "api/v1/cedar/job/"+jobId+"/?fields="+
+                        "id,"+
+                        "author,author.username,"+
+                        "android_version,android_version.version,"+
+                        "rom_version,rom_version.version,"+
+                        "description,"+
+                        "updated_time,"+
+                        "custom_tag,custom_tag.custom_tag_name,"+
+                        "phone_models,phone_models.phone_model_name,"+
+                        "test_area,test_area.description,"+
+                        "job_name,"+
+                        "job_label"
+                    )
+                    .then(response=>{
+                        this.jobInfo = utils.validate(serializer, response.data);
+                        let versions = [];
+                        this.jobInfo.android_version.forEach(version=>{
+                            versions.push(version.version)
+                        })
+                        this.androidVersion = versions.join(",");
+
+                        versions=[];
+                        this.jobInfo.rom_version.forEach(version=>{
+                            versions.push(version.version)
+                        })
+                        this.romVersion = versions.join(",");
+
+                        let tags = [];
+                        this.jobInfo.custom_tag.forEach(tag=>{
+                            tags.push(tag.version)
+                        })
+                        this.customTag = tags.join(",");
+
+                        let models=[];
+                        this.jobInfo.phone_models.forEach(model=>{
+                            models.push(model.version)
+                        })
+                        this.phoneModels = models.join(",");
+
+                        let areas=[];
+                        this.jobInfo.test_area.forEach(area=>{
+                            areas.push(area.version)
+                        })
+                        this.testArea = areas.join(",");
+
+                    })
+                    .catch(error=>{
+                        this.$Message.error("数据加载失败");
+                    })
             }
         }
     }
