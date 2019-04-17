c9f10ad6ce9a642511c7f69899122865ffc392c2
tingting <1140115803@qq.com>
2019-04-17 18:11:05 +0800

    增加系统升级页面

---
+++ src/views/SystemUpgrade.vue
@@ -0,0 +1,36 @@
+<template>
+    <Upload :data="updateData" :action="updateUrl" :on-error="handleUploadError" :on-success="handleUploadSuccess">
+        <Button icon="ios-cloud-upload-outline">导入系统升级包</Button>
+    </Upload>
+</template>
+
+<script>
+    import utils from "../lib/utils"
+    import config from "../lib/config"
+
+    export default {
+        data(){
+            return{
+                updateData:{
+                    requestName:"setNewZip"
+                },
+                updateUrl:""
+            }
+        },
+        methods:{
+            handleUploadError(){
+
+            },
+            handleUploadSuccess(){
+
+            },
+        },
+        created(){
+            this.updateUrl = utils.getCoralUrl(config.ADMIN_PORT)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
