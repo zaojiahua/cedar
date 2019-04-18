3898d21d98393535ae1a25ebe53e1e6bfe5452a7
tingting <1140115803@qq.com>
2019-04-18 11:19:33 +0800

    增加一个不带动画效果的CompTooltip组件，用于rds查看数据

---
+++ src/components/CompTooltip.vue
@@ -0,0 +1,29 @@
+<template>
+    <div :class="[prefixCls]" @mouseenter="handleShowPopper" @mouseleave="handleClosePopper">
+        <div :class="[prefixCls + '-rel']" ref="reference">
+            <slot></slot>
+        </div>
+        <div
+            :class="[prefixCls + '-popper', prefixCls + '-' + theme]"
+            :style="dropStyles"
+            ref="popper"
+            v-show="!disabled && (visible || always)"
+            @mouseenter="handleShowPopper"
+            @mouseleave="handleClosePopper"
+            :data-transfer="transfer"
+            v-transfer-dom>
+            <div :class="[prefixCls + '-content']">
+                <div :class="[prefixCls + '-arrow']"></div>
+                <div :class="innerClasses" :style="innerStyles"><slot name="content">{{ content }}</slot></div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import {Tooltip} from 'iview';
+
+    export default {
+        name: 'CompTooltip',
+        mixins: [Tooltip],
+    };
+</script>
