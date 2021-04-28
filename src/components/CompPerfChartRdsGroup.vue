ef3a2b01e9b0e68cebd37037aa75ff432efa1367
ltt <ltt@wtovc.com>
2021-04-28 11:22:14 +0800

    perfDataView 性能图表和rds数据封装成一个组件，避免数据相互影响

---
+++ src/components/CompPerfChartRdsGroup.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <comp-perf-histogram style="margin-top: 20px;" ref="histogram" :job-id="job.job_id"
+                             @on-chart-click="onChartClick" @after-load-data="afterLoadData"
+        ></comp-perf-histogram>
+
+        <comp-perf-rds-list ref="perfRdsList" v-if="timeRange" :prop-job-id="job.job_id" :prop-tboard-id="tboardId" :prop-time-range="timeRange"></comp-perf-rds-list>
+    </div>
+</template>
+
+<script>
+    import CompPerfHistogram from "../components/CompPerfHistogram";
+    import CompPerfRdsList from "../components/CompPerfRdsList";
+
+    export default {
+        name: "CompPerfChartRdsGroup",
+        components:{ CompPerfHistogram, CompPerfRdsList },
+        props:{
+            job:{
+                type: Object,
+                default: ()=>{
+                    return {}
+                }
+            },
+            tboardId:{
+                type: Number,
+                default: null
+            }
+        },
+        data(){
+            return{
+                timeRange:""
+            }
+        },
+        methods:{
+            afterLoadData(item){
+                this.timeRange = item
+            },
+            onChartClick(item){
+                this.timeRange = item
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
