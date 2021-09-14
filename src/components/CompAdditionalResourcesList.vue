6f8929ef97a43c004d831889112c0c751479a28e
ltt <ltt@wtovc.com>
2021-09-14 19:01:40 +0800

    feature：资源导入导出excel

--- src/components/CompAdditionalResourcesList.vue
+++ src/components/CompAdditionalResourcesList.vue
@@ -1,19 +1,39 @@
 <template>
     <div style="margin-left: 2px">
-        <Row>
-            <Select v-model="tableList" style="width: 200px;margin-bottom: 16px;">
-                <Option :value="1">SIM卡资源列表</Option>
-                <Option :value="2">账号资源列表</Option>
-            </Select>
-            <Button v-show="(tableList===1)&&(username==='admin')" type="primary" style="float: right;" @click="onOpenSimModal">添加SIM卡资源</Button>
-            <Button v-show="(tableList===2)&&(username==='admin')" type="primary" style="float: right;" @click="onOpenAppModal">添加账号资源</Button>
+        <Row style="margin-bottom: 16px">
+            <Col span="12">
+                <Select v-model="tableList" style="width: 200px;">
+                    <Option :value="1">SIM卡资源列表</Option>
+                    <Option :value="2">账号资源列表</Option>
+                </Select>
+                <Button v-show="(tableList===1)&&(username==='admin')" type="primary" style="margin-left: 16px" @click="onOpenSimModal">添加SIM卡资源</Button>
+                <Button v-show="(tableList===2)&&(username==='admin')" type="primary" style="margin-left: 16px" @click="onOpenAppModal">添加账号资源</Button>
+            </Col>
+
+            <Col span="12" style="text-align: right" v-show="username==='admin'">
+                <Button type="warning" style="margin-right: 16px;" @click="cancelSelected">取消选择 ( {{ selectedNum }} )</Button>
+                <Dropdown trigger="click" style="margi n-right: 2px;">
+                    <Button>
+                        更多操作
+                        <Icon type="ios-arrow-down"></Icon>
+                    </Button>
+                    <DropdownMenu slot="list" style="text-align: left">
+                        <Upload :action="importResourcesUrl" name="import_file" :show-upload-list="false"
+                                :on-success="importResourcesSuccess" :on-error="importResourcesFailed">
+                            <DropdownItem>导入资源</DropdownItem>
+                        </Upload>
+                        <span @click="exportResources"><DropdownItem>导出资源</DropdownItem></span>
+                        <span @click="deleteMore"><DropdownItem>批量删除</DropdownItem></span>
+                    </DropdownMenu>
+                </Dropdown>
+            </Col>
         </Row>
 
         <div v-if="tableList===1">
-            <comp-sim-list ref="simTable"></comp-sim-list>
+            <comp-sim-list :prop-multi-select="true" ref="simTable" @selected-count="getSelectedNum"></comp-sim-list>
         </div>
         <div v-if="tableList===2">
-            <comp-app-table  ref="appTable"></comp-app-table>
+            <comp-app-table :prop-multi-select="true" ref="appTable" @selected-count="getSelectedNum"></comp-app-table>
         </div>
 
         <Modal v-model="showAddSimModal" :mask-closable="false" :footer-hide="true">
@@ -32,6 +52,7 @@
     import CompSimList from "./CompSimList";
     import CompAddSimMsg from "./CompAddSimCardMsg"
     import CompAddAppCardMsg from "./CompAddAppCardMsg"
+    import config from "../lib/config"
 
     export default {
         components:{ CompAppTable, CompSimList, CompAddSimMsg, CompAddAppCardMsg,  },
@@ -41,6 +62,8 @@
                 showAddSimModal:false,
                 showAddAppModal:false,
                 username:"",
+                selectedNum:0,
+                importResourcesUrl:"http://"+config.REEF_HOST+":"+config.REEF_PORT + "/api/v1/cedar/resource_import/",
             }
         },
         methods:{
@@ -59,6 +82,176 @@
             onAfterAddApp(){
                 this.showAddAppModal = false;
                 this.$refs.appTable.getData()
+            },
+            //取得已选数量
+            getSelectedNum(number){
+                this.selectedNum = number
+            },
+            //取消全选
+            cancelSelected(){
+                if(this.tableList===1){  //sim
+                    this.$refs.simTable.resetSimList()
+                }else if(this.tableList===2) {   //app
+                    this.$refs.appTable.resetAppList()
+                }
+            },
+            //批量删除
+            deleteMore(){
+                if(this.tableList===1){  //sim
+                    let sims = this.$refs.simTable.getThisSelection()
+                    if(sims.length===0){
+                        this.$Message.info("请选择要删除的SIM卡资源！")
+                        return
+                    }
+                    let _this = this
+                    this.$Modal.confirm({
+                        title:"警告！",
+                        content:"确认要删除已选择的资源吗？",
+                        onOk(){
+                            let ajaxObj = [];
+                            sims.forEach(info=>{
+                                ajaxObj.push(_this.$ajax.delete("api/v1/cedar/simcard/" + info.id + "/"))
+                            })
+                            _this.$ajax.all(ajaxObj)
+                                .then(response=>{
+                                    _this.$Message.success("SIM卡资源删除成功！")
+                                    _this.$refs.simTable.resetSimList()
+                                    _this.$refs.simTable.getData()
+
+                                }).catch(error=>{
+                                    if (config.DEBUG) console.log(error)
+                                    _this.$refs.simTable.resetSimList()
+                                    _this.$Message.error({content:"SIM卡资源删除失败;"+error.response.data.message,duration:7})
+                            })
+                        }
+                    })
+                }else if(this.tableList===2) {   //app
+                    let apps = this.$refs.appTable.getThisSelection()
+                    if(apps.length===0){
+                        this.$Message.info("请选择要删除的APP账号资源！")
+                        return
+                    }
+                    let _this = this
+                    this.$Modal.confirm({
+                        title:"警告！",
+                        content:"确认要删除已选择的资源吗？",
+                        onOk(){
+                            let ajaxObj = [];
+                            apps.forEach(info=>{
+                                ajaxObj.push(_this.$ajax.delete("api/v1/cedar/account/" + info.id + "/"))
+                            })
+                            _this.$ajax.all(ajaxObj)
+                                .then(response=>{
+                                    _this.$Message.success("app账号资源删除成功！")
+                                    _this.$refs.appTable.resetAppList()
+                                    _this.$refs.appTable.getData()
+                                }).catch(error=>{
+                                if (config.DEBUG) console.log(error)
+                                    _this.$refs.appTable.resetAppList()
+                                    _this.$Message.error({content:"app账号资源删除失败;"+error.response.data.message,duration:7})
+                            })
+                        }
+                    })
+                }
+            },
+            //导入资源 success/ fail
+            importResourcesSuccess(response, file, fileList){
+                this.$Message.success("资源导入成功！")
+                if(this.tableList===1) {  //sim
+                    this.$refs.simTable.resetSimList()
+                    this.$refs.simTable.getData()
+                }else if(this.tableList===2) {   //app
+                    this.$refs.appTable.resetAppList()
+                    this.$refs.appTable.getData()
+                }
+            },
+            importResourcesFailed(error, file, ){  // 两个参数对应的是=>Error/response
+                if (config.DEBUG) console.log(error);
+                if(this.tableList===1) {  //sim
+                    this.$refs.simTable.resetSimList()
+                }else if(this.tableList===2) {   //app
+                    this.$refs.appTable.resetAppList()
+                }
+                this.$Notice.error({
+                    title: '资源导入失败！',
+                    desc:  file.data_info.join("<br>"),
+                    duration: 60
+                });
+            },
+            //导出资源数据
+            exportResources(){
+                if(this.tableList===1){  //sim
+                    let sims = this.$refs.simTable.getThisSelection()
+                    if(sims.length===0){
+                        this.$Message.info("请选择需要导出的SIM卡资源！")
+                        return
+                    }
+                    let simIds = []
+                    sims.forEach(info=>{
+                        simIds.push(info.id)
+                    })
+                    console.log(sims)
+                    let _this = this
+                    this.$Modal.confirm({
+                        title:"提示！",
+                        content:"确认要导出已选择的SIM卡资源吗？",
+                        onOk(){
+                            _this.$ajax.post("api/v1/cedar/resource_export/",{
+                                resource_type: "SIMCard",
+                                sim_card: simIds
+                            }).then(response=>{
+                                console.log(response.data)
+                                window.open("http://"+config.REEF_HOST+":"+config.REEF_PORT + response.data)
+                                _this.$Message.success("正在导出...")
+                                _this.$refs.simTable.resetSimList()
+                                _this.$refs.simTable.getData()
+                            }).catch(error=>{
+                                if (config.DEBUG) console.log(error)
+                                _this.$refs.simTable.resetSimList()
+                                _this.$Message.error({content:"SIM卡资源导出失败;"+error.response.data.message,duration:7})
+                            })
+                        }
+                    })
+
+                }else if(this.tableList===2) {   //app
+                    let apps = this.$refs.appTable.getThisSelection()
+                    if(apps.length===0){
+                        this.$Message.info("请选择需要导出的APP账号资源！")
+                        return
+                    }
+                    let appsIds = []
+                    apps.forEach(info=>{
+                        appsIds.push(info.id)
+                    })
+                    let _this = this
+                    this.$Modal.confirm({
+                        title:"提示！",
+                        content:"确认要导出已选择的账号资源吗？",
+                        onOk(){
+                            _this.$ajax.post("api/v1/cedar/resource_export/",{
+                                resource_type: "Account",
+                                account: appsIds
+                            }).then(response=>{
+                                window.open("http://"+config.REEF_HOST+":"+config.REEF_PORT + response.data)
+                                _this.$Message.success("正在导出...")
+                                _this.$refs.appTable.resetAppList()
+                                _this.$refs.appTable.getData()
+                            }).catch(error=>{
+                                if (config.DEBUG) console.log(error)
+                                _this.$refs.appTable.resetAppList()
+                                _this.$Message.error({content:"app账号资源导出失败;"+error.response.data.message,duration:7})
+                            })
+                        }
+                    })
+                }
+            }
+        },
+        watch:{
+            tableList:{
+                handler: function(val){
+                    this.selectedNum = 0
+                },
+                immediate: true
             }
         },
         mounted(){
@@ -67,8 +260,14 @@
     }
 </script>
 
+<style>
+    .ivu-notice{
+        width: 385px!important;
+    }
+</style>
 <style scoped>
     /deep/.ivu-modal-close{
         margin: 18px;
     }
+
 </style>
